e084d8e1edf262af102a0ed3487f5bcd8d8d53dd
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-05 03:01:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) && (index <= 9 && index >= 1)
+  if position_taken?(board, index) == false && (index <= 9 && index >= 1)
     true
   else
     false
